6a5f22d983e6fa7b3bb295651130c07121b156a0
CSimonini <73425362+CSimonini@users.noreply.github.com>
2024-04-22 16:16:23 +0200

    Update W02_Int_Streams.sql

--- W02_Int_Streams.sql
+++ W02_Int_Streams.sql
@@ -1,80 +1,82 @@
+use role securityadmin;
+
+grant usage on warehouse compute_wh to role sysadmin;
+
 use role sysadmin;
 
-create or replace database frosty_friday;
-use database frosty_friday;
-create or replace schema w2_intermediate;
+-- create or replace database FROSTY_FRIDAYS;
+use database FROSTY_FRIDAYS;
+create or replace schema WEEK_2;
 
-use warehouse compute_wh;
-
-create or replace file format ff_parquet
-  TYPE = parquet
-;
-
--- MAIN URL = 'https://frostyfridaychallenges.s3.eu-west-1.amazonaws.com/challenge_2/employees.parquet'
-create or replace stage w2_stage
+-- Original URL https://frostyfridaychallenges.s3.eu-west-1.amazonaws.com/challenge_2/employees.parquet
+create or replace stage FF_W2_STAGE
     url = 's3://frostyfridaychallenges/challenge_2'
-    file_format = ff_parquet
 ;
 
--- check the results
-list @w2_stage;
+-- check what's in the stage and in a file
+list @FF_W2_STAGE;
 
--- create the row table in order to import the parquet file
-create or replace table W2_Intermediate.Row_Table (
-    RAW_LOG Variant
+-- create file format
+create or replace file format FF_W2_FF_PARQUET
+    type = parquet
+;
+
+-- create the table
+create or replace table FF_W2_TABLE_ROW (
+    RAW_LOG variant
 );
 
-copy into row_table
- from @w2_stage
- FILES = ('/employees.parquet')
- file_format = (format_name = ff_parquet)
- ;
+COPY INTO FF_W2_TABLE_ROW
+    FROM @FF_W2_STAGE
+    files = ('/employees.parquet')
+    file_format = (format_name = FF_W2_FF_PARQUET)
+;
 
  -- Check the result
-select $1 from row_table;
-
+select $1 from FF_W2_TABLE_ROW;
 
 -- flatten for creating the regular table
-create or replace table employees as (
-select
-    f1.$1:city::varchar as city
-    ,f1.$1:country::varchar as country
-    ,f1.$1:country_code::varchar as country_code
-    ,f1.$1:dept::varchar as dept
-    ,f1.$1:education::varchar as education
-    ,f1.$1:email::varchar as email
-    ,f1.$1:employee_id::varchar as employee_id
-    ,f1.$1:first_name::varchar as first_name
-    ,f1.$1:job_title::varchar as job_title
-    ,f1.$1:last_name::varchar as last_name
-    ,f1.$1:payroll_iban::varchar as payroll_iban
-    ,f1.$1:postcode::varchar as postcode
-    ,f1.$1:street_name::varchar as street_name
-    ,f1.$1:time_zone::varchar as time_zone
-    ,f1.$1:title::varchar as title
-from row_table f1
+create or replace table FF_W2_TABLE as (
+    select
+        f1.$1:city::varchar as city
+        ,f1.$1:country::varchar as country
+        ,f1.$1:country_code::varchar as country_code
+        ,f1.$1:dept::varchar as dept
+        ,f1.$1:education::varchar as education
+        ,f1.$1:email::varchar as email
+        ,f1.$1:employee_id::varchar as employee_id
+        ,f1.$1:first_name::varchar as first_name
+        ,f1.$1:job_title::varchar as job_title
+        ,f1.$1:last_name::varchar as last_name
+        ,f1.$1:payroll_iban::varchar as payroll_iban
+        ,f1.$1:postcode::varchar as postcode
+        ,f1.$1:street_name::varchar as street_name
+        ,f1.$1:time_zone::varchar as time_zone
+        ,f1.$1:title::varchar as title
+    from FF_W2_TABLE_ROW f1
 );
 
 -- Check the result -> 100 rows
-select * from employees;
+select * from FF_W2_TABLE;
 
-create or replace view employees_view as (
+-- create the subtable
+create or replace view FF_W2_VIEW as (
     select 
         employee_id
         ,dept
         ,job_title 
-    from employees
+    from FF_W2_TABLE
 );
 
 -- create the stream
-create or replace stream employees_stream on view employees_view;
+create or replace stream FF_W2_STREAM on view FF_W2_VIEW;
 
 -- run the requested commands for the challenge
-UPDATE employees SET COUNTRY = 'Japan' WHERE EMPLOYEE_ID = 8;
-UPDATE employees SET LAST_NAME = 'Forester' WHERE EMPLOYEE_ID = 22;
-UPDATE employees SET DEPT = 'Marketing' WHERE EMPLOYEE_ID = 25;
-UPDATE employees SET TITLE = 'Ms' WHERE EMPLOYEE_ID = 32;
-UPDATE employees SET JOB_TITLE = 'Senior Financial Analyst' WHERE EMPLOYEE_ID = 68;
+UPDATE FF_W2_TABLE SET COUNTRY = 'Japan' WHERE EMPLOYEE_ID = 8;
+UPDATE FF_W2_TABLE SET LAST_NAME = 'Forester' WHERE EMPLOYEE_ID = 22;
+UPDATE FF_W2_TABLE SET DEPT = 'Marketing' WHERE EMPLOYEE_ID = 25;
+UPDATE FF_W2_TABLE SET TITLE = 'Ms' WHERE EMPLOYEE_ID = 32;
+UPDATE FF_W2_TABLE SET JOB_TITLE = 'Senior Financial Analyst' WHERE EMPLOYEE_ID = 68;
 
 -- Check the final result on the stream
-select * from employees_stream;
+select * from FF_W2_STREAM;
