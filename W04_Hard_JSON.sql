c29e8dfb99838c9a1e4f9e0c27590e5b6c228cb7
CSimonini <73425362+CSimonini@users.noreply.github.com>
2024-04-23 11:49:08 +0200

    Update W04_Hard_JSON.sql

--- W04_Hard_JSON.sql
+++ W04_Hard_JSON.sql
@@ -1,66 +1,73 @@
-use database frosty_friday;
-create schema W4_row;
+-- use role securityadmin;
 
---  MAIN URL - https://frostyfridaychallenges.s3.eu-west-1.amazonaws.com/challenge_4/Spanish_Monarchs.json
-create or replace stage FF_W4_Stage
-    url = 's3://frostyfridaychallenges/challenge_4';
+-- grant usage on warehouse compute_wh to role sysadmin;
 
--- Check the files in the stage
-list @FF_W4_Stage;
+use role sysadmin;
 
--- Create file format
+-- create or replace database FROSTY_FRIDAYS;
+use database FROSTY_FRIDAYS;
+create or replace schema WEEK_4;
+use schema WEEK_4;
+
+-- Original URL https://frostyfridaychallenges.s3.eu-west-1.amazonaws.com/challenge_2/employees.parquet
+create or replace stage FF_W4_STAGE
+    url = 's3://frostyfridaychallenges/challenge_4'
+;
+
+-- check what's in the stage and in a file
+list @FF_W4_STAGE;
+
+-- create file format
 create or replace file format FF_JSON
     type = 'JSON'
     compression = 'auto'
-    strip_outer_array = TRUE;
+    strip_outer_array = TRUE -- Boolean that instructs the JSON parser to remove outer brackets (i.e. [ ])
+;
 
--- Create ROW Table
-create or replace table FROSTY_FRIDAY.W4_ROW.Frost_Row (
-    RAW_LOG Variant
+-- create and populate the table
+create or replace table FF_W4_TABLE_ROW (
+    RAW_LOG variant
 );
 
--- Create File Format
-copy into FROSTY_FRIDAY.W4_ROW.Frost_Row
- from @FF_W4_Stage
- FILES = ('/Spanish_Monarchs.json')
- file_format = (format_name = FF_JSON);
- 
+COPY INTO FF_W4_TABLE_ROW
+    FROM @FF_W4_STAGE
+    files = ('/Spanish_Monarchs.json')
+    file_format = (format_name = FF_JSON)
+;
+
 -- Check the result
-select $1 from FROSTY_FRIDAY.W4_ROW.Frost_Row;
+select $1 from FF_W4_TABLE_ROW;
 
--- Create the right schema
-create schema FROSTY_FRIDAY.W4;
-
-create or replace table FROSTY_FRIDAY.W4.Frost as
-select
-    row_number()
-        over(order by f3.value:Birth::varchar) as ID,
-    row_number() 
-        over(partition by f2.value:House::varchar 
-            order by f3.value:Birth::varchar) as INTER_HOUSE_ID,
-    f1.$1:Era::varchar as Era,
-    f2.value:House::varchar as House,
-    f3.value:Name::varchar as Name,
-    f3.value:Nickname[0]::varchar as Nickname_1,
-    f3.value:Nickname[1]::varchar as Nickname_2,
-    f3.value:Nickname[2]::varchar as Nickname_3,
-    f3.value:Birth::varchar as Birth,
-    f3.value:"Place of Birth"::varchar as Place_of_Birth,
-    f3.value:"Start of Reign"::varchar as "Start of Reign",
-    f3.value:"Consort\/Queen Consort"[0]::varchar as "Queen or Queen Consort_1",
-    f3.value:"Consort\/Queen Consort"[1]::varchar as "Queen or Queen Consort_2",
-    f3.value:"Consort\/Queen Consort"[2]::varchar as "Queen or Queen Consort_3",
-    f3.value:"End of Reign"::varchar as "End of Reign",
-    f3.value:"Duration"::varchar as "Duration",
-    f3.value:"Death"::varchar as "Death",
-    f3.value:"Age at Time of Death"::varchar as "Age at Time of Death",
-    f3.value:"Place of Death"::varchar as "Place of Death",
-    f3.value:"Burial Place"::varchar as "Burial Place"
-from FROSTY_FRIDAY.W4_ROW.Frost_Row f1,
-lateral flatten(input => f1.$1:Houses) f2,
-lateral flatten(input => f2.value:Monarchs) f3
-order by 1,2
+create or replace table FF_W4_TABLE as
+    select
+         row_number()
+            over(order by f3.value:Birth::varchar) as ID
+        ,row_number() 
+            over(partition by f2.value:House::varchar 
+                order by f3.value:Birth::varchar) as INTER_HOUSE_ID
+        ,f1.$1:Era::varchar as Era
+        -- ,f2.value:House::varchar as House
+        ,f3.value:Name::varchar as Name
+        ,f3.value:Nickname[0]::varchar as Nickname_1
+        ,f3.value:Nickname[1]::varchar as Nickname_2
+        ,f3.value:Nickname[2]::varchar as Nickname_3
+        ,f3.value:Birth::varchar as Birth
+        ,f3.value:"Place of Birth"::varchar as Place_of_Birth
+        ,f3.value:"Start of Reign"::varchar as "Start of Reign"
+        ,f3.value:"Consort\/Queen Consort"[0]::varchar as "Queen or Queen Consort_1"
+        ,f3.value:"Consort\/Queen Consort"[1]::varchar as "Queen or Queen Consort_2"
+        ,f3.value:"Consort\/Queen Consort"[2]::varchar as "Queen or Queen Consort_3"
+        ,f3.value:"End of Reign"::varchar as "End of Reign"
+        ,f3.value:"Duration"::varchar as "Duration"
+        ,f3.value:"Death"::varchar as "Death"
+        ,f3.value:"Age at Time of Death"::varchar as "Age at Time of Death"
+        ,f3.value:"Place of Death"::varchar as "Place of Death"
+        ,f3.value:"Burial Place"::varchar as "Burial Place"
+    from FF_W4_TABLE_ROW f1,
+    lateral flatten(input => f1.$1:Houses) f2,
+    lateral flatten(input => f2.value:Monarchs) f3
+    order by 1,2
 ;
 
 -- check the result
-select * from FROSTY_FRIDAY.W4.FROST;
+select * from FF_W4_TABLE;
